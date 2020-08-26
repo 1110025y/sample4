3add430cfdc90749331f0a4851e7674ecdaa80c3
1110025y <spw57h9@gmail.com>
2020-08-26 23:14:22 +0900

    update7

--- app/assets/javascripts/top-image.js
+++ app/assets/javascripts/top-image.js
@@ -1,37 +1,4 @@
-$(document).on('turbolinks:load', function(){
-  $(function() {
-  
-    // アップロードするファイルを選択
-    $('#file-sample').change(function() {
-      var file = $(this).prop('files')[0];
-  
-      // 画像表示
-      var reader = new FileReader();
-      reader.onload = function() {
-        var src = reader.result
-        var img_src = `<div class='item-image' >
-                        <div class=' item-image__content'>
-                          <div class='item-image__content--icon'>
-                            <img src=${src} width="200" height="150" class='picture'>
-                          </div>
-                        </div>
-                        <div class='item-image__operetion'>
-                          <div class='item-image__operetion--delete' >削除</div>
-                        </div>
-                      </div>`
-        $('#image-boxer').html(img_src);
-      }
-      reader.readAsDataURL(file);
-    });
-  });
-});
-
-
-
-
 $(function(){
-
-
   //（１）ページの概念・初期ページを設定
   var page=0;
   
