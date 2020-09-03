ae3896a47935dd228ab1874998cc5d2e09d9d7af
1110025y <spw57h9@gmail.com>
2020-09-03 13:44:43 +0900

    uodate21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,11 @@ class UsersController < ApplicationController
   def new
   end
   
-  def create
-    @user = User.create(user_registration_params)
-    @nickname = current_user.nickname
-  end
+  # def create
+  #   @user = User.create(user_registration_params)
+  #   @nickname = current_user.nickname
+  # end
 
-  def edit
-  end
-
-  def update
-  end
 
   def show
     @items = Item.all
@@ -29,8 +24,8 @@ class UsersController < ApplicationController
     @address = current_user.address
   end
 
-  private
-  def user_registration_params
-    params.require(:user).permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana)
-  end
+  # private
+  # def user_registration_params
+  #   params.require(:user).permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana)
+  # end
 end
